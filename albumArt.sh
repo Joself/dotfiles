6ccabfc65f4fe1f68c3d5e8942c013217ce2a40e
Joself <joselfwithm@gmail.com>
2017-12-30 18:51:24 +0100

    Delete albumArt.sh

--- albumArt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$(playerctl status)" = "Playing" ]; then
-	url=$(playerctl metadata mpris:artUrl)
-	url="i.scdn.co$(sed 's|https://open.spotify.com||g' <<< $url)"
-	echo $url
-	wget $url -O ~/scripts/currentArt
-fi
